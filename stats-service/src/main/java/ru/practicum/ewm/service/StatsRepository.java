da86cb3865a11df0faeacabea3197b53bf702e2a
egospegos <belyakov1998@mail.ru>
2023-09-13 15:11:37 +0300

    Диплом. 1й этап, статистика. Desc

--- stats-service/src/main/java/ru/practicum/ewm/service/StatsRepository.java
+++ stats-service/src/main/java/ru/practicum/ewm/service/StatsRepository.java
@@ -20,7 +20,8 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query(value = "select h.app as app, h.uri as uri, COUNT(distinct h.ip) as hits " +
             "from hits as h " +
             "where h.uri IN (?3) AND h.created >= ?1 AND h.created <= ?2 " +
-            "group by h.app, h.uri", nativeQuery = true)
+            "group by h.app, h.uri " +
+            "order by hits DESC", nativeQuery = true)
     List<IViewStats> findByUriWithUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uri);
 
     @Query(value = "select h.app as app, h.uri as uri, COUNT(h.ip) as hits " +
@@ -32,6 +33,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query(value = "select h.app as app, h.uri as uri, COUNT(h.ip) as hits " +
             "from hits as h " +
             "where h.uri IN (?3) AND h.created >= ?1 AND h.created <= ?2 " +
-            "group by h.app, h.uri", nativeQuery = true)
+            "group by h.app, h.uri " +
+            "order by hits DESC", nativeQuery = true)
     List<IViewStats> findByUriWithNonUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uri);
 }
